56de3616a469a400b1da3eb0f9608649d1cde800
Airidas Juska <airidas665@gmail.com>
2023-01-31 15:24:37 +0200

    task 1, half 2nd done

--- 001/app.js
+++ 001/app.js
@@ -1,9 +1,26 @@
 
-const arr = []
+//TASK 1
 
-for(let i = 0; i < 10; i++) {
-    arr.push(Math.floor(Math.random() * (10 - 1 + 1)) + 1)
-    
-}
+const arr1 = [...Array(30)].map(_ => Math.floor(Math.random() * (25 - 5) + 5))
+console.log(arr1)
+// console.log("TASK 1: generate 30 random numbers(5-25) to an array  ",arr1)
 
-console.log(arr)
+//TASK 2a
+const aboveTen = arr1.reduce(
+    (accumulator, n) => accumulator + (n > 10 ? 1 : 0),
+    0
+  );
+// console.log("How many numbers are above ten?", aboveTen)
+
+//TASK 2b
+const highestNum = arr1.reduce((a, b) => Math.max(a, b))
+// console.log("Highest number of the array is: ", highestNum)
+
+//TASK 2c
+const sumOfEven = arr1.reduce((acc, curr) => acc + (curr % 2 === 0 ? curr : 0))
+// console.log("Sum of every even array element: ", sumOfEven)
+
+//TASK 2d
+const arr1Changed = arr1.map((curr, index) => curr - index)
+// console.log("Every element from arr1 minus its index (arr[1] - 1)", arr1Changed)
+
