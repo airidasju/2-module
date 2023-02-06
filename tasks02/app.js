e378c6545aa4e66591e04103801ef1e8eaee986a
Airidas Juska <airidas665@gmail.com>
2023-02-07 00:23:19 +0200

    task 2

--- tasks02/app.js
+++ tasks02/app.js
@@ -1,7 +1,38 @@
+const getRandomNum = (max, min) =>
+  Math.floor(Math.random() * (max - min) + min);
+
 //TASK 1
-const diceRoll = (( diceOne = Math.floor(Math.random() * (7 - 1) + 1)) +  (diceTwo = Math.floor(Math.random() * (7 - 1) + 1))) > 8 ? `Win! Dice one: ${diceOne} | Dice two: ${diceTwo}` : `Loss! Dice one: ${diceOne} | Dice two: ${diceTwo}`
+const diceRoll =
+  (diceOne = getRandomNum(7, 1)) + (diceTwo = getRandomNum(7, 1)) > 8
+    ? `Win! Dice one: ${diceOne} | Dice two: ${diceTwo}`
+    : `Loss! Dice one: ${diceOne} | Dice two: ${diceTwo}`;
 // console.log(diceRoll)
 //If you roll more than 8 from two dice, you win.
 
-
 //TASK 2
+const cats = [
+  {
+    name: 'Rexas',
+    weight: getRandomNum(7, 3),
+  },
+  {
+    name: 'Boltas',
+    weight: getRandomNum(7, 3),
+  },
+];
+
+const compareCats = cats.reduce((acc, curr) =>
+  acc.weight == curr.weight
+    ? `${cats
+        .map((el) => `${el.name} ${el.weight}kg\n`)
+        .join('')}\nTheir weight is equal`
+    : acc.weight < curr.weight
+    ? `${cats.map((el) => `${el.name} ${el.weight}kg\n`).join('')}\n${
+        acc.name
+      } is lighter`
+    : `${cats.map((el) => `${el.name} ${el.weight}kg\n`).join('')}\n${
+        curr.name
+      } is lighter`,
+);
+console.log(cats);
+console.log(compareCats);
