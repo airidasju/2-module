66db20438ce49e027bc6ce817202e3308bf2b6b9
Airidas Juska <airidas665@gmail.com>
2023-02-07 15:31:48 +0200

    005

--- 005/app.js
+++ 005/app.js
@@ -1,6 +1,5 @@
 console.log('Hello 005');
 
-
 const obj = {};
 
 const arr = [];
@@ -8,56 +7,48 @@ const arr = [];
 const map = new Map();
 
 class SayName {
+  static voice = 'big one';
 
-    static voice = 'big one';
+  static whatIsYourVoice() {
+    return this.voice; // this -> klase
+  }
 
-    static whatIsYourVoice() {
-        return this.voice; // this -> klase
-    }
+  whatIsYourVoiceNotStatic() {
+    return this.constructor.voice;
+    return this.voice; // this -> objektas
+  }
 
-    whatIsYourVoiceNotStatic() {
-        return this.constructor.voice;
-        return this.voice; // this -> objektas
+  constructor() {
+    this.papers = 'Yes';
+  }
 
-    }
-
-    constructor() {
-        this.papers = 'Yes';
-    }
-
-    whatIsYourName() {
-        return this.name;
-    }
-
+  whatIsYourName() {
+    return this.name;
+  }
 }
 
-
 class Cat extends SayName {
+  static voice = 'small one';
 
-    static voice = 'small one';
+  constructor(name, color) {
+    super();
+    this.name = name;
+    this.color = color;
+    this.age = parseInt(Math.random() * 10 + 1);
+  }
 
-    constructor(name, color) {
-        super();
-        this.name = name;
-        this.color = color;
-        this.age = parseInt(Math.random() * 10 + 1);
-    }
-
-    whatIsYourName() {
-        return 'Miau';
-    }
-
+  whatIsYourName() {
+    return 'Miau';
+  }
 }
 
 class Dog extends SayName {
-
-    constructor(name) {
-        super();
-        this.name = name;
-        this.age = parseInt(Math.random() * 15 + 1);
-        this.papers = 'No';
-    }
-
+  constructor(name) {
+    super();
+    this.name = name;
+    this.age = parseInt(Math.random() * 15 + 1);
+    this.papers = 'No';
+  }
 }
 
 const murka = new Cat('Mūrka', 'brown');
@@ -66,12 +57,9 @@ const pilkis = new Dog('Pilkis');
 // console.log(pilkis.whatIsYourName());
 // console.log(murka.whatIsYourName());
 
-
 console.log(murka.whatIsYourVoiceNotStatic());
 console.log(SayName.whatIsYourVoice());
 console.log(Cat.whatIsYourVoice());
-
-
 
 // console.log(obj);
 // console.log(arr);
