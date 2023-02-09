42a42bf8e3a970c19a87d6b70b4742986d2670a8
Airidas Juska <airidas665@gmail.com>
2023-02-09 17:48:59 +0200

    task 2 complete

--- tasks03/app.js
+++ tasks03/app.js
@@ -1,7 +1,13 @@
+function rand(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
 //TASK 1
+const raceLaps = 10
 
 const raceCounter = () => {
-  let raceLaps = 10
     for(let i = 0; i < raceLaps; i++) {
     console.log(raceLaps-i)
   }
@@ -9,3 +15,13 @@ const raceCounter = () => {
 // raceCounter()
 
 //TASK 2
+const avgSpeed = () => {
+  const speedEachLap = []
+  for(let i = 0; i < 10; i++) {
+    speedEachLap.push(rand(120, 220))
+  }
+  const lapsAvg = speedEachLap.reduce((acc, curr) => acc + curr)/10
+  console.log("The racer was averaging",lapsAvg,"km/h")
+  return lapsAvg
+}
+avgSpeed()
