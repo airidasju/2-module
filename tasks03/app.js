bdb4d6de8990d74ea6a19ca69d4da327e9885a65
Airidas Juska <airidas665@gmail.com>
2023-02-10 17:35:25 +0200

    task 4 + fixes

--- tasks03/app.js
+++ tasks03/app.js
@@ -31,14 +31,42 @@ const avgSpeed = () => {
 const fuelCounter = () => {
   let fuelRemaining = 44
   for(let i = 1; i <= 10; i ++) {
-    fuelRemaining = fuelRemaining - rand(3, 6)
+    const fuelCost = rand(3, 6)
+    fuelRemaining = fuelRemaining - fuelCost
     if(fuelRemaining < 3 && fuelRemaining < 0) {
-      console.log("Not enough fuel to finish the lap",i,"\n", fuelRemaining,"litres remaining")
-      return fuelRemaining
+      console.log("Not enough fuel to finish the lap",i,"\nWe currently have",fuelRemaining+fuelCost,"litres", 'and we need', fuelCost, '\n\nWe are short on', Math.abs(fuelRemaining),'liter')
+      return fuelRemaining+fuelCost
     }
-    console.log(fuelRemaining, "after lap",i)
   }
   console.log("There was enough fuel for the race", fuelRemaining,"litres remaining")
   return fuelRemaining
 }
-fuelCounter()
+// fuelCounter()
+
+
+//TASK 4
+
+const fastestTurn = () => {
+  const turns = []
+  const turnInfo =[]
+  for(let i = 0; i < 10; i++) {
+    for(let t = 0; t < 5; t++) {
+      const randomTurn = rand(20, 120)
+      if (turns.length == 0) {
+        turns.push(randomTurn)
+        turnInfo.push(["Lap",i+1,"Turn",t+1,"Speed", randomTurn])
+      }
+      // console.log(randomTurn, turns[turns. length - 1])
+        if(randomTurn >  turns[turns. length - 1]) {
+          turns.push(randomTurn)
+          turnInfo.push(["lap",i+1,"turn",t+1,"speed", randomTurn])
+        }
+        // console.log("lap",i+1,"\n","turn",t+1,"\n","speed", randomTurn)
+      }
+    }
+    console.log(`The fastest turn happened on lap ${turnInfo[turnInfo. length - 1][1]} turn ${turnInfo[turnInfo. length - 1][3]} with a speed of ${turns[turns. length - 1]} km/h`)
+    return turns[turns. length - 1]
+}
+// fastestTurn()
+
+
