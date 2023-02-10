b95d9b398c11890575d17d31bd02d9148fd379c2
Airidas Juska <airidas665@gmail.com>
2023-02-10 13:56:30 +0200

    task 3

--- tasks03/app.js
+++ tasks03/app.js
@@ -7,12 +7,12 @@ function rand(min, max) {
 //TASK 1
 const raceLaps = 10
 
-const raceCounter = () => {
+const lapCounter = () => {
     for(let i = 0; i < raceLaps; i++) {
     console.log(raceLaps-i)
   }
 }
-// raceCounter()
+// lapCounter()
 
 //TASK 2
 const avgSpeed = () => {
@@ -24,4 +24,21 @@ const avgSpeed = () => {
   console.log("The racer was averaging",lapsAvg,"km/h")
   return lapsAvg
 }
-avgSpeed()
+// avgSpeed()
+
+
+//TASK 3
+const fuelCounter = () => {
+  let fuelRemaining = 44
+  for(let i = 1; i <= 10; i ++) {
+    fuelRemaining = fuelRemaining - rand(3, 6)
+    if(fuelRemaining < 3 && fuelRemaining < 0) {
+      console.log("Not enough fuel to finish the lap",i,"\n", fuelRemaining,"litres remaining")
+      return fuelRemaining
+    }
+    console.log(fuelRemaining, "after lap",i)
+  }
+  console.log("There was enough fuel for the race", fuelRemaining,"litres remaining")
+  return fuelRemaining
+}
+fuelCounter()
