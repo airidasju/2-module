f7b43ba731593100e0dd3e33ee153a7cf339436e
Airidas Juska <airidas665@gmail.com>
2023-02-13 14:14:35 +0200

    buttons

--- react1/src/App.jsx
+++ react1/src/App.jsx
@@ -3,44 +3,38 @@ import './App.scss';
 import AnimalLi from './components/008/AnimalLi';
 import Click from './components/008/Click';
 
-
 const data = [
-    {id: 1, animal: "Racoon", color: 'crimson', bold:'true'},
-    {id: 4, animal: "Fox", color: 'brown', bold:'true'},
-    {id: 17, animal: "Beaver", color: 'skyblue', bold: 'false'},
-    {id: 3, animal: "Unicorn", color: 'coral', bold:'true'},
-]
-
+    { id: 1, animal: 'Racoon', color: 'crimson', bold: true },
+    { id: 4, animal: 'Fox', color: 'brown', bold: true },
+    { id: 17, animal: 'Beaver', color: 'skyblue', bold: false },
+    { id: 3, animal: 'Unicorn', color: 'coral', bold: true }
+];
 
 function App() {
 
+    return (
+        <div className="App">
+            <header className="App-header">
 
-  return (
-    <div className='App'>
-      <header className='App-header'>
-            <div className='App'>
+            <Click/>
 
-                    <Click></Click>
-                    
-                <div className='card'>
-                    <div className='card-header'>
+                {/* <div className="card">
+                    <div className="card-header">
                         <h2>Animals List</h2>
                     </div>
-
-
-                    <ul className='list-group list-group-flush '>
+                    
+                    <ul className="list-group list-group-flush">
                         {
-
-                            data.map((a, i) => a = <AnimalLi key={a.id} animal={a} index={i}></AnimalLi>)
-
+                            data.map((a, i) => <AnimalLi key={a.id} animal={a} index={i} />)
                         }
                     </ul>
-                </div>
+                </div> */}
 
-            </div>
-      </header>
-    </div>
-  );
+
+            </header>
+        </div>
+    );
+
 }
 
 export default App;
