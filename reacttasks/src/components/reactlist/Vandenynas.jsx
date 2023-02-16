b4abc37d2dc334d37cda20f750f57b3a01b68ce3
Airidas Juska <airidas665@gmail.com>
2023-02-16 20:04:02 +0200

    format

--- reacttasks/src/components/reactlist/Vandenynas.jsx
+++ reacttasks/src/components/reactlist/Vandenynas.jsx
@@ -3,22 +3,24 @@ import Tipas from './Tipas';
 import Vardas from './Vardas';
 import Spalva from './Spalva';
 
-function Vandenynas({sea}) {
-    
-    const seaByType = [...sea].sort((a, b) => a.type > b.type ? 1 : -1)
-    const seaByName = [...sea].sort((a, b) => a.name > b.name ? 1 : -1)
-    const seaByColor = [...sea].sort((a, b) => a.color > b.color ? 1 : -1)
+function Vandenynas({ sea }) {
+  const seaByType = [...sea].sort((a, b) => (a.type > b.type ? 1 : -1));
+  const seaByName = [...sea].sort((a, b) => (a.name > b.name ? 1 : -1));
+  const seaByColor = [...sea].sort((a, b) => (a.color > b.color ? 1 : -1));
 
-    return (
-        <div>
-            <ul className='board'>
-                {
-                //    seaByType.map((el) => <Tipas element={el} key={uuidv4()}></Tipas>)}
-                   seaByName.map((el) => <Vardas element={el} key={uuidv4()}></Vardas>)}
-                   {/* seaByColor.map((el) => <Spalva element={el} key={uuidv4()}></Spalva>)} */}
-            </ul>
-        </div>
-    )
+  return (
+    <div>
+      <ul className='board'>
+        {
+          //    seaByType.map((el) => <Tipas element={el} key={uuidv4()}></Tipas>)}
+          seaByName.map((el) => (
+            <Vardas element={el} key={uuidv4()}></Vardas>
+          ))
+        }
+        {/* seaByColor.map((el) => <Spalva element={el} key={uuidv4()}></Spalva>)} */}
+      </ul>
+    </div>
+  );
 }
 
 export default Vandenynas;
