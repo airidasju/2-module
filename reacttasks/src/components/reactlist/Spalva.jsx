b0b92b0863792669512553ab408e15f369dd9770
Airidas Juska <airidas665@gmail.com>
2023-02-16 20:03:51 +0200

    format

--- reacttasks/src/components/reactlist/Spalva.jsx
+++ reacttasks/src/components/reactlist/Spalva.jsx
@@ -1,11 +1,7 @@
 import { v4 as uuidv4 } from 'uuid';
 
-function Spalva({element}) {
-
-    return (
-        <li key={uuidv4()}>{Object.values(element).map((el) => el+" ")}</li>
-)
-
+function Spalva({ element }) {
+  return <li key={uuidv4()}>{Object.values(element).map((el) => el + ' ')}</li>;
 }
 
 export default Spalva;
