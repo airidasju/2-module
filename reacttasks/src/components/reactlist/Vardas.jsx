6cf5a5ac8adbf9eadc435a219ca19e0ee6d85dc1
Airidas Juska <airidas665@gmail.com>
2023-02-16 20:03:42 +0200

    format

--- reacttasks/src/components/reactlist/Vardas.jsx
+++ reacttasks/src/components/reactlist/Vardas.jsx
@@ -1,11 +1,7 @@
 import { v4 as uuidv4 } from 'uuid';
 
-function Vardas({element}) {
-
-    return (
-        <li key={uuidv4()}>{Object.values(element).map((el) => el+" ")}</li>
-)
-
+function Vardas({ element }) {
+  return <li key={uuidv4()}>{Object.values(element).map((el) => el + ' ')}</li>;
 }
 
 export default Vardas;
