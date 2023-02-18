f14cd7f8b5ca01931c232da89cb135438d114bf9
Airidas Juska <airidas665@gmail.com>
2023-02-18 14:59:50 +0200

    task 5

--- reacttasks/src/components/reactlist/Namas.jsx
+++ reacttasks/src/components/reactlist/Namas.jsx
@@ -1,12 +1,7 @@
 import { v4 as uuidv4 } from 'uuid';
 
-function Namas({element}) {
-
-    return (
-        <li key={uuidv4()}>{Object.values(element).map((el) => el + " ")}</li>
-)
-
-
+function Namas({ element }) {
+  return <li key={uuidv4()}>{Object.values(element).map((el) => el + ' ')}</li>;
 }
 
 export default Namas;
