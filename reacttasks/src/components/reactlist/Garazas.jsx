38231830c959a680311df1ba4d9ac95fc08fb21e
Airidas Juska <airidas665@gmail.com>
2023-02-18 15:00:05 +0200

    task 5

--- reacttasks/src/components/reactlist/Garazas.jsx
+++ reacttasks/src/components/reactlist/Garazas.jsx
@@ -1,11 +1,7 @@
 import { v4 as uuidv4 } from 'uuid';
 
-function Garazas({element}) {
-
-    return (
-        <li key={uuidv4()}>{Object.values(element).map((el) => el + " ")}</li>
-)
-
+function Garazas({ element }) {
+  return <li key={uuidv4()}>{Object.values(element).map((el) => el + ' ')}</li>;
 }
 
 export default Garazas;
