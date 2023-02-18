9fc8bd05a9bb6f8372de7f534855412cc5d35701
Airidas Juska <airidas665@gmail.com>
2023-02-18 14:59:57 +0200

    task 5

--- reacttasks/src/components/reactlist/Narvas.jsx
+++ reacttasks/src/components/reactlist/Narvas.jsx
@@ -1,11 +1,7 @@
 import { v4 as uuidv4 } from 'uuid';
 
-function Narvas({element}) {
-
-    return (
-        <li key={uuidv4()}>{Object.values(element).map((el) => el + " ")}</li>
-)
-
+function Narvas({ element }) {
+  return <li key={uuidv4()}>{Object.values(element).map((el) => el + ' ')}</li>;
 }
 
 export default Narvas;
