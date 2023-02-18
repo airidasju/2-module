5180329326763537012f2caeda19a40e29d38f65
Airidas Juska <airidas665@gmail.com>
2023-02-18 15:00:13 +0200

    task 5

--- reacttasks/src/components/reactlist/Akvariumas.jsx
+++ reacttasks/src/components/reactlist/Akvariumas.jsx
@@ -1,11 +1,7 @@
 import { v4 as uuidv4 } from 'uuid';
 
-function Akvariumas({element}) {
-
-    return (
-        <li key={uuidv4()}>{Object.values(element).map((el) => el + " ")}</li>
-)
-
+function Akvariumas({ element }) {
+  return <li key={uuidv4()}>{Object.values(element).map((el) => el + ' ')}</li>;
 }
 
 export default Akvariumas;
