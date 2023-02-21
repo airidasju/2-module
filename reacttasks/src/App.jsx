a431cf0df760bd15100b4bf02fc40a2e494b6660
Airidas Juska <airidas665@gmail.com>
2023-02-21 19:05:12 +0200

    task 1

--- reacttasks/src/App.jsx
+++ reacttasks/src/App.jsx
@@ -1,33 +1,21 @@
 import './App.scss';
 import { v4 as uuidv4 } from 'uuid';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(() => {
-    // getting stored value
-    const saved = localStorage.getItem('count');
-    const initialValue = JSON.parse(saved);
-    return initialValue || 0;
-  });
+  const [form, setChecked] = useState(true);
 
-  useEffect(() => {
-    // storing input name
-    localStorage.setItem('count', count);
-  }, [count]);
-
-  const addOne = () => {
-    setCount((c) => c + 1);
-  };
+  const toggleChecked = () => setChecked((val) => !val);
 
   return (
     <div className='App'>
       <header className='App-header'>
-        <div className='bin'>
-          <button className='button' onClick={() => addOne()}>
-            +{count}
-          </button>
-        </div>
+        <button onClick={() => toggleChecked()}>Click</button>
+        <div
+          className={`transformation`}
+          style={{ borderRadius: form ? '50%' : null }}
+        ></div>
       </header>
     </div>
   );
