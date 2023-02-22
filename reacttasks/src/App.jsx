aef880536bbe9905eb09be579238fd89389b671f
Airidas Juska <airidas665@gmail.com>
2023-02-22 14:34:40 +0200

    task 2

--- reacttasks/src/App.jsx
+++ reacttasks/src/App.jsx
@@ -2,23 +2,40 @@ import './App.scss';
 import { v4 as uuidv4 } from 'uuid';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useState } from 'react';
+import rand from './functions/rand';
 
 function App() {
-  const [form, setChecked] = useState({num: 5, rectangle: false});
+  const [form, setChecked] = useState({ num: rand(5, 25), rectangle: false });
 
-  const toggleChecked = () => setChecked((prevState ) => [{num: 5, rectangle: !prevState.rectangle}]);
+  const changeForm = () => {
+    setChecked((prevState) => {
+      return {
+        ...prevState,
+        rectangle: !prevState.rectangle,
+      };
+    });
+  };
 
-
+  const changeNum = () => {
+    setChecked((prevState) => {
+      return {
+        ...prevState,
+        num: rand(5, 25),
+      };
+    });
+  };
 
   return (
     <div className='App'>
       <header className='App-header'>
-        <button onClick={() => toggleChecked()}>Change form</button>
-        <button >Random num 5-25</button>
+        <button onClick={() => changeForm()}>Change form</button>
+        <button onClick={() => changeNum()}>Random num 5-25</button>
         <div
           className={`transformation`}
           style={{ borderRadius: form.rectangle ? '50%' : null }}
-        ></div>
+        >
+          {form.num}
+        </div>
       </header>
     </div>
   );
