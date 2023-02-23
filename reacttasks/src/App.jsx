148248337d6c7c34fcbc38a1ebbdedf5e60f4451
Airidas Juska <airidas665@gmail.com>
2023-02-23 15:26:48 +0200

    task 4, 5

--- reacttasks/src/App.jsx
+++ reacttasks/src/App.jsx
@@ -3,15 +3,18 @@ import { v4 as uuidv4 } from 'uuid';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useState } from 'react';
 import SimpleSquare from './components/reactstate/SimpleSquare';
+import rand from "./functions/rand"
 
 function App() {
   const [sq, setSq] = useState([]);
 
-  const addSq = () => {
+  const addSq = (color) => {
     setSq((s) => [
       ...s,
       {
         id: uuidv4(),
+        color: color,
+        index: rand(1, 999)
       },
     ]);
   };
@@ -19,11 +22,11 @@ function App() {
   return (
     <div className='App'>
       <header className='App-header'>
-        <button onClick={() => addSq()}>Plus sq</button>
-
+        <button onClick={() => addSq("crimson")}>Add red</button>
+        <button onClick={() => addSq("indigo")}>Add indigo</button>
         <div className={`canvas`}>
           {sq.map((s) => (
-            <SimpleSquare key={uuidv4()} sq={s} setSq={setSq}></SimpleSquare>
+            <SimpleSquare key={uuidv4()} sq={s} setSq={setSq} i={s.index} color={s.color}></SimpleSquare>
           ))}
         </div>
       </header>
--- reacttasks/src/components/reactstate/SimpleSquare.jsx
+++ reacttasks/src/components/reactstate/SimpleSquare.jsx
@@ -1,11 +1,12 @@
-function SimpleSquare({ sq, setSq }) {
+function SimpleSquare({ sq, setSq, i, color}) {
   const delSq = () => {
     setSq((s) => s.filter((s) => s.id !== sq.id));
   };
 
   return (
-    <div className='simpleSq'>
+    <div className='simpleSq' style={{backgroundColor: color}}>
       <button className='delButton' onClick={delSq}></button>
+      <span style={{color: 'white'}}>{i}</span>
     </div>
   );
 }
